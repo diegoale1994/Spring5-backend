bf715c546dae4105388a8f22be331803ee35baa0
Diego Alejandro <diego.fc.1@hotmail.com>
2019-03-08 23:58:41 -0500

    agregada configuracion spring security Auth0

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,3 +17,14 @@ INSERT INTO regiones (id,nombre) VALUES (5, 'Asia');
 INSERT INTO regiones (id,nombre) VALUES (6, 'Africa');
 INSERT INTO regiones (id,nombre) VALUES (7, 'Oceania');
 INSERT INTO regiones (id,nombre) VALUES (8, 'Antartida');
+
+INSERT INTO `usuarios` (username, password, enabled) VALUES ('diego','$2a$10$c.OXMl22oBdSK8spNXohxeTh0T4sjuvGPdZhZ4O2RNMrDdWuN6tjy',1);
+INSERT INTO `usuarios` (username, password, enabled) VALUES ('admin','$2a$10$rSdhNkyWuL9xJ9nT2fUTyexNhXiVu8li2FTbOsm8J/T4lxgOj7Qey',1);
+
+INSERT INTO `roles` (nombre) VALUES ('ROLE_USER');
+INSERT INTO `roles` (nombre) VALUES ('ROLE_ADMIN');
+
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (1,1);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2,2);
+INSERT INTO `usuarios_roles` (usuario_id, role_id) VALUES (2,1);
+
