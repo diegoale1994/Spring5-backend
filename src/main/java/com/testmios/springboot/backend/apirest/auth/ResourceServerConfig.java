3532e8bb43695de6fdf2946882138a67918efbd7
Diego Alejandro <diego.fc.1@hotmail.com>
2019-03-21 22:18:26 -0500

    proyecto terminado -Seguridad reseteada

--- src/main/java/com/testmios/springboot/backend/apirest/auth/ResourceServerConfig.java
+++ src/main/java/com/testmios/springboot/backend/apirest/auth/ResourceServerConfig.java
@@ -22,8 +22,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/clientes","/api/clientes/page/**", "/api/uploads/img/**", "/img/**").permitAll()
-		.antMatchers("/api/clientes/{id}").permitAll()
-		.antMatchers("/api/facturas/**").permitAll()
+		//.antMatchers("/api/clientes/{id}").permitAll()
+		//.antMatchers("/api/facturas/**").permitAll()
 		/*.antMatchers(HttpMethod.GET,"/api/clientes/{id}").hasAnyRole("USER","ADMIN")
 		.antMatchers(HttpMethod.POST,"/api/clientes/upload").hasAnyRole("USER","ADMIN")
 		.antMatchers(HttpMethod.POST,"/api/clientes").hasRole("ADMIN")
@@ -35,7 +35,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); //*
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*")); //*
         configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT","DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
--- src/main/java/com/testmios/springboot/backend/apirest/controllers/FacturaRestController.java
+++ src/main/java/com/testmios/springboot/backend/apirest/controllers/FacturaRestController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,22 +27,29 @@ public class FacturaRestController {
 	@Autowired
 	private IClienteService clienteService;
 	
+	@Secured({"ROLE_ADMIN","ROLE_USER"})
 	@GetMapping("/facturas/id/{id}")
 	@ResponseStatus(code=HttpStatus.OK)
 	public Factura show(@PathVariable("id") Long id) {
 		return clienteService.findFacturaById(id);
 	}
-	
+	@Secured({"ROLE_ADMIN"})
 	@DeleteMapping("/facturas/id/{id}")
 	@ResponseStatus(code=HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") Long id) {
 		clienteService.deleteFacura(id);
 	}
-	
+	@Secured({"ROLE_ADMIN"})
 	@GetMapping("/facturas/filtrar-productos/{termino}")
 	@ResponseStatus(code=HttpStatus.OK)
 	public List<Producto> filtrarProductors(@PathVariable("termino") String termino){
 		return clienteService.findByNombreProducto(termino);
+	}
+	@Secured({"ROLE_ADMIN"})
+	@PostMapping("/facturas")
+	@ResponseStatus(code=HttpStatus.CREATED)
+	public Factura crearFactura(@RequestBody Factura factura) {
+		return clienteService.saveFactura(factura);
 	}
 	
 }
